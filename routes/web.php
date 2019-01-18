e959a198ab3c7187a50ed1a3b44c9f94f3403050
yunie <2859913655@qq.com>
2019-01-18 11:02:42 +0800

    更新个人资料

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,8 @@ Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
 
-Route::get('signup  ', 'UserController@create')->name('signup');
+//注册
+Route::get('signup', 'UserController@create')->name('signup');
 
 Route::resource('users', 'UserController');
 
