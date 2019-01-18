3c30676cb931bdd9f33149914f6feb1b3f94b281
yunie <2859913655@qq.com>
2019-01-18 10:27:10 +0800

    用户退出登录

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -20,7 +20,7 @@ class SessionsController extends Controller
         if (\Auth::attempt($credentials)) {
             // 登录成功后的相关操作
             session()->flash('success', '欢迎回来！');
-            return redirect()->route('users.show',[\Auth::user()]);
+            return redirect()->route('users.show', [\Auth::user()]);
 
         } else {
             // 登录失败后的相关操作
@@ -31,6 +31,8 @@ class SessionsController extends Controller
 
     public function destroy()
     {
-
+        \Auth::logout();
+        session()->flash('success', '你也成功退出！');
+        return redirect('login');
     }
 }
