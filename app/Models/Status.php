ee759e509b51b7668e25d0fd3ae264541edf92f7
yunie <2859913655@qq.com>
2019-01-25 21:54:10 +0800

    发布微博

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Status extends Model
 {
+
+    protected $fillable = ['content'];
+
+
     public function user()
     {
 //        在用户模型中，指明一个用户拥有多条微博。
